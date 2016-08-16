fe813ab7d5a5bba089e6f4cf5d5550cffa42e4de
DeclanMcDaid <l00099356@student.lyit.ie>
2016-08-16 13:50:18 +0100

    comment for the craic

---
+++ src/Grades.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+/**
+ * @author l00099356
+ *
+ */
+public class Grades {
+	private int studentid = 123456;
+	private String moduleid = "ENTPERF";
+	private double grade = 0.0;
+	
+	public double getGrade() {
+	return grade;
+	}
+	public void setGrade(double grade) 
+	{
+	this.grade = grade;//done this for the craic
+	}
+	public int getStudentid() 
+	{
+	return studentid;
+	}
+	public String getModuleid() 
+	{
+	return moduleid;
+	}
+	public int  getGrade1(){
+		return 15;
+	}
+	
+
+}
+
